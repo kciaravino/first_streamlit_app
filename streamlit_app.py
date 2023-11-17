7feebbdb3798441eadf04ccf225abfe85202cc93
kciaravino <113860205+kciaravino@users.noreply.github.com>
2023-11-17 09:46:49 -0700

    Update streamlit_app.py
    add menu special

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,5 @@ streamlit.text('🥣 Omega 3 and blueberry oatmeal')
 streamlit.text('🥗 Kale, spinach, and rocket smoothie')
 streamlit.text('🐔 Hard boiled free range egg')
 streamlit.text('🥑🍞 Avocado toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
